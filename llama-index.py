42e4288b7e58498054a67c65c75429ff77d36bc0
Augusto Bernardes <augustobdev@outlook.com>
2026-03-17 11:31:56 -0300

    feat: testing and learning how to use tools llama

--- llama-index.py
+++ llama-index.py
@@ -1,16 +1,24 @@
 import asyncio
 
+
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core.ingestion import IngestionPipeline
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import VectorStoreIndex
+from llama_index.core.tools import QueryEngineTool
+
 
 import chromadb
 from llama_index.vector_stores.chroma import ChromaVectorStore
 
 from llama_index.llms.huggingface_api import HuggingFaceInferenceAPI
 
+from llama_index.tools.google import GmailToolSpec
+
+# External community tool
+tool_spec = GmailToolSpec()
+tool_spec_list = tool_spec.to_tool_list()
 
 async def main():
 
@@ -26,7 +34,7 @@ async def main():
     # Ingestion pipeline (Split chuncks)
     pipeline = IngestionPipeline(
         transformations=[
-            SentenceSplitter(chunk_size=25, chunk_overlap=0),
+            SentenceSplitter(chunk_size=512, chunk_overlap=50),
             HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5"),
         ],
         vector_store=vector_store,
@@ -50,13 +58,21 @@ async def main():
     )
 
     # Query engine
-    query_engine = index.as_query_engine(
-        llm=llm,
-        response_mode="tree_summarize",
+    query_engine = index.as_query_engine(llm=llm)
+    # By a tool i can ask an agent and he decide if will use or not
+    hockey_games_tool = QueryEngineTool.from_defaults(
+        query_engine,
+        name="nhl_games_search",
+        description="Use this tool to answer questions about NHL hockey games, teams, matches, and results"
     )
+    
+    # Testing NHl tool
+    tool_response = hockey_games_tool.call("What are the current teams on playoffs ?")
 
-    response = query_engine.query("Which teams fought in a war in New York?")
-    print(response)
+
+    # Using query i call directly the engine RAG to check my data
+    #response = query_engine.query("Which teams fought in a war in New York?")
+    print(tool_response)
 
 
 asyncio.run(main())
